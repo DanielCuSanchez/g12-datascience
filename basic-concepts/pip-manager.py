c511f6f66e8b765cc5dca5424582f136e853b545
DanielCu <danielcu@alternet.com.mx>
2022-09-29 21:13:18 -0500

    Challenge

---
+++ basic-concepts/pip-manager.py
@@ -0,0 +1,5 @@
+from simple_colors import *
+
+print(red('hello'))
+print(green('hello', 'bold'))
+print(blue('hello', ['bold', 'underlined']))
---
+++ basic-concepts/user-input.py
@@ -0,0 +1,11 @@
+
+
+from asyncio import protocols
+from traceback import print_tb
+
+# User input and cast values
+
+number = int(input("Ingresa un numero: "))
+
+print(type(number))
+print(number)
